4e6e38a76a918cf5295e277ad8766c29af3394c1
Ana Luiza Favacho <analufavacho@gmail.com>
2024-10-29 20:09:40 -0300

    add toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import LoginPage from "./pages/auth/Login";
 import RegisterPage from "./pages/auth/Register";
 import LoginLayout from "./components/layouts/LoginLayout";
 import { ToastContainer } from "react-toastify";
-import { NotFound } from './pages/statusCode/NotFound'
+import { NotFound } from './pages/statusCode/NotFound';
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <>
-      <ToastContainer position="bottom-right" theme="colored" />
-      <Routes>
-        <Route element={<LoginLayout />}>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <SnackbarProvider>
+                <Routes>
+                    <Route element={<LoginLayout />}>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </SnackbarProvider>
+        </>
+    );
 }
 
 export default App;
